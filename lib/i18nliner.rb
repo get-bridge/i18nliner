704ae0ba9f226497c2e937da31a92bfc0583729d
Joey Marianer <rmarianer@instructure.com>
2018-05-23 11:30:05 -0700

    Compatibility with i18n 1.0.1

--- lib/i18nliner.rb
+++ lib/i18nliner.rb
@@ -1,8 +1,8 @@
 require 'i18n'
 require 'i18nliner/base'
-
 require 'i18nliner/extensions/core'
+
 I18n.send :extend, I18nliner::Extensions::Core
-I18n::RESERVED_KEYS << :i18nliner_scope << :i18nliner_inferred_key
+I18n::RESERVED_KEYS = I18n.send(:remove_const, :RESERVED_KEYS) + [:i18nliner_scope, :i18nliner_inferred_key]
 
 require 'i18nliner/railtie' if defined?(Rails)
